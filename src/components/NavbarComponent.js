c895bcdd268f6057b12fdc6a8fa23edcfa83fbfd
D Pringle <n305oul93@gmail.com>
2021-02-14 12:56:45 -0600

    Creates the navbar component

--- src/components/NavbarComponent.js
+++ src/components/NavbarComponent.js
@@ -8,10 +8,69 @@ import {
   NavItem
 } from 'reactstrap';
 import { NavLink } from 'react-router-dom';
+import '../css/Navbar.css';
 
 class MainNavbar extends Component {
+  constructor(props) {
+    super(props);
+    this.toggleNav = this.toggleNav.bind(this);
+    this.state = {
+      isNavOpen: false
+    };
+  }
+
+  toggleNav() {
+    this.setState({ isNavOpen: !this.state.isNavOpen });
+  }
+
   render() {
-    return <h1>Navbar</h1>;
+    return (
+      <Navbar light expand='md'>
+        <div className='container'>
+          <NavbarBrand href='/'>Sportsfan Café</NavbarBrand>
+          <NavbarToggler onClick={this.toggleNav} />
+          <Collapse isOpen={this.state.isNavOpen} navbar>
+            <Nav navbar className='ml-auto'>
+              <NavItem>
+                <NavLink className='nav-link' to='/'>
+                  Menu
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink className='nav-link' to='/'>
+                  Pricing
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink className='nav-link' to='/'>
+                  Room Rentals
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink className='nav-link' to='/'>
+                  Photos
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink className='nav-link' to='/'>
+                  Blog
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink className='nav-link' to='/'>
+                  About
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink className='nav-link' to='/'>
+                  Request a tour
+                </NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </div>
+      </Navbar>
+    );
   }
 }
 
